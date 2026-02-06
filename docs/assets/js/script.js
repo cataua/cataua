94fa42eb46e785da1564238ff2782836217114aa
Rogério Caetano <cataua+github@gmail.com>
2026-02-05 22:53:35 -0300

    style: improve code formatting and consistency in translation script

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,25 +1,23 @@
-(function() {
+(function () {
     const button = document.querySelectorAll('[data-action="translate"]');
-    button.forEach(function(btn) {
+    button.forEach(function (btn) {
         btn.addEventListener('click', (e) => translate(e));
     });
 })();
 function translate(button) {
-    const languages = [ 'en', 'pt'];
+    const languages = ['en', 'pt'];
     const lang = button.target.getAttribute('data-language');
     const buttons = document.querySelectorAll('.btn');
-    buttons.forEach(function(btn) {
+    buttons.forEach(function (btn) {
         btn.classList.toggle('hide');
     });
-    languages.forEach(function(language) {
-        const elements = document.querySelectorAll(`#${language}`);
-        elements.forEach(function(element) {
-            const id = element.getAttribute('id');
-            if (language !== id) {
-                element.classList.add('hide');
-            } else {
-                element.classList.remove('hide');
-            }
-        });
+    languages.forEach(function (language) {
+        const element = document.querySelector(`#${language}`);
+        const id = element.getAttribute('id');
+        if (lang !== id) {
+            element.classList.add('hide');
+        } else {
+            element.classList.remove('hide');
+        }
     });
 }
