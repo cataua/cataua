eb2aba878032145eb46f8a474fa7a3d8e1fded78
Rogério Caetano <cataua+github@gmail.com>
2026-02-06 23:26:52 -0300

    feat: add styles, SVG assets, and update script for language translation and menu toggle
    - Created a new SCSS file for styling with variables, base styles, typography, layout, and utility classes.
    - Added new SVG assets for header graphics and language icons (English and Portuguese).
    - Updated JavaScript to include a menu toggle functionality and improved language translation handling.
    - Refactored HTML structure for better semantic organization and accessibility.

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,23 +1,34 @@
 (function () {
-    const button = document.querySelectorAll('[data-action="translate"]');
-    button.forEach(function (btn) {
-        btn.addEventListener('click', (e) => translate(e));
-    });
+  const button = document.querySelectorAll('[data-action="translate"]');
+  const btnToggleMenu = document.querySelector('[data-action="menu-toggle"]');
+  if (btnToggleMenu) {
+    btnToggleMenu.addEventListener("click", (e) => toggleMenu(e));
+  }
+  button.forEach(function (btn) {
+    btn.addEventListener("click", (e) => translate(e));
+  });
 })();
+
 function translate(button) {
-    const languages = ['en', 'pt'];
-    const lang = button.target.getAttribute('data-language');
-    const buttons = document.querySelectorAll('.btn');
-    buttons.forEach(function (btn) {
-        btn.classList.toggle('hide');
-    });
-    languages.forEach(function (language) {
-        const element = document.querySelector(`#${language}`);
-        const id = element.getAttribute('id');
-        if (lang !== id) {
-            element.classList.add('hide');
-        } else {
-            element.classList.remove('hide');
-        }
-    });
+  const languages = ["en", "pt"];
+  const lang = button.target.getAttribute("data-language");
+  const buttons = document.querySelectorAll('[data-action="translate"]');
+  buttons.forEach(function (btn) {
+    btn.classList.toggle("hide");
+  });
+  languages.forEach(function (language) {
+    const element = document.querySelector(`#${language}`);
+    const id = element.getAttribute("id");
+    if (lang !== id) {
+      element.classList.add("hide");
+    } else {
+      element.classList.remove("hide");
+    }
+  });
+}
+
+function toggleMenu(event) {
+  const targetSelector = event.currentTarget.getAttribute("data-target");
+  const menu = document.querySelector(targetSelector);
+  menu.classList.toggle("hide");
 }
